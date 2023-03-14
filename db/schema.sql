ac2c5b5db67cdb232122d34202409de4c0a956f4
moviefan322 <philman202@yahoo.com>
2023-03-14 17:35:14 -0400

    adds schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,32 @@
-DROP DATABASE IF EXISTS todos_db;
-CREATE DATABASE todos_db;
+DROP DATABASE IF EXISTS hw_db;
+CREATE DATABASE hw_db;
 
-USE todos_db;
+USE hw_db;
 
-CREATE TABLE todos (
-  name VARCHAR(30) NOT NULL,
-  complete BOOLEAN NOT NULL DEFAULT false
-)
+CREATE TABLE department (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    title VARCHAR(30),
+    salary DECIMAL NOT NULL,
+    department_id INT,
+     FOREIGN KEY (department)
+    REFERENCES department(id)
+    ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    FOREIGN KEY (role)
+    REFERENCES role(id),
+    manager_id INT NOT NULL,
+    FOREIGN KEY
+    REFERENCES employee(id)
+    ON DELETE SET NULL
+);
